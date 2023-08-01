6f59c79ae2c63a0466a3ee702e4e569324d0507a
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-01 14:01:03 +0530

    port change 3011

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3011;
 
 app.use(express.static('public'));
 
